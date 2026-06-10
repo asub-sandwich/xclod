45915b366d103a61267064629719d604e4770c89
asub-sandwich <adam.subora@proton.me>
2026-06-10 14:59:42 -0500

    vid2jpg working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![allow(warnings)]
 use anyhow::{Result, bail};
 use clap::{Parser, Subcommand};
+use indicatif::ProgressBar;
 use which::which;
 
-use std::fs::create_dir;
+use std::ffi::OsStr;
+use std::fs::{create_dir, create_dir_all, read_dir};
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::thread;
 
 const DONE_MSG: &'static str = "\ndone!\n";
 
@@ -17,17 +20,23 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// runs vid2jpg
+    /// converts a video to several JPGs
     Vid2jpg {
-        /// path to the input video
+        /// filename of the video
         input: PathBuf,
-        /// path to the "site" directory (i.e. `KS1`)
+        /// name of the site folder (i.e. `KS1`)
         output: PathBuf,
         /// name of the sample (i.e. `KS1-H1-3`)
         sample_name: String,
         /// frames to extract per second of video
-        #[arg(default_value_t = 3)]
+        #[arg(short, long, default_value_t = 3)]
         fps: u8,
+        /// directory containing the input video file (shouldn't have to change)
+        #[arg(short, long, default_value = OsStr::new("/mnt/c/Users/65610791/Pictures/BulkDensity_2026"))]
+        input_dir: PathBuf,
+        /// directory containting the output root directory (shouldn't have to change)
+        #[arg(short, long, default_value = OsStr::new("/home/ada/bulkdensity/jpgs"))]
+        output_dir: PathBuf,
     },
 }
 
@@ -42,8 +51,12 @@ fn main() -> Result<()> {
             output,
             sample_name,
             fps,
+            input_dir,
+            output_dir,
         } => {
-            vid2jpg(input, output, sample_name, fps);
+            let input = input_dir.join(&input);
+            let output = output_dir.join(&output);
+            vid2jpg(&input, &output, sample_name, fps)?;
         }
     }
     Ok(())
@@ -66,6 +79,8 @@ fn check_dependencies() -> Result<()> {
         for tool in missing {
             println!("{}", tool);
         }
+
+        bail!("unable to continue without dependencies!");
     }
 
     Ok(())
@@ -76,15 +91,10 @@ fn vid2jpg(input_path: &Path, output_root: &Path, sample_name: &str, fps: &u8) -
         bail!("{} doesn't exist!", input_path.display());
     }
 
-    if !output_root.is_dir() {
-        create_dir(output_root)?;
-    }
-
     let output_dir = output_root.join(sample_name);
+    create_dir_all(&output_dir)?;
 
-    if !output_dir.is_dir() {
-        create_dir(&output_dir)?;
-    }
+    println!("\nextracting frames with ffmpeg...");
 
     let arg_input = format!("{}", input_path.display());
     let arg_fps = format!("fps={}", fps);
@@ -126,44 +136,68 @@ fn vid2jpg(input_path: &Path, output_root: &Path, sample_name: &str, fps: &u8) -
         if !stdout.is_empty() {
             println!("ffmpeg:\n{}", stdout);
         }
+        println!("ffmpeg done!\n");
     } else {
         let stderr = String::from_utf8_lossy(&ffmpeg.stderr);
         bail!("ffmpeg error:\n{}", stderr);
     }
 
-    let arg_exif_output = format!("{}/frame_*.jpg", output_dir.display());
-
-    let exif_args = [
-        "-quiet",
-        "-overwrite_original",
-        "-ee",
-        "-TagsFromFile",
-        &arg_input,
-        "-all:all",
-        "-unsafe",
-        "-icc_profile",
-        &arg_exif_output,
-    ];
-
-    let exiftool = Command::new("exiftool")
-        .args(exif_args)
-        .output()
-        .expect("failed to run exiftool!");
-
-    if exiftool.status.success() {
-        let stdout = String::from_utf8_lossy(&exiftool.stdout);
-        if !stdout.is_empty() {
-            println!("exiftool:\n{}", stdout);
-        }
-    } else {
-        let stderr = String::from_utf8_lossy(&exiftool.stderr);
-        bail!("exiftool error:\n{}", stderr);
-    }
+    let jpg_paths: Vec<PathBuf> = read_dir(&output_dir)?
+        .filter_map(|e| {
+            let path = e.expect("failure parsing output dir paths").path();
+            match path.extension() {
+                Some(ext) if ext.eq_ignore_ascii_case("jpg") => Some(path),
+                _ => None,
+            }
+        })
+        .collect();
 
     println!(
-        "\nframes written to {}, additional metadata copied via exiftool. done!\n",
-        output_dir.display()
+        "copying metadata with exiftool ({} frames)...",
+        jpg_paths.len()
     );
+
+    let num_jobs = jpg_paths.len();
+
+    let bar = ProgressBar::new(num_jobs.try_into()?);
+
+    let mut handles = vec![];
+
+    for jpg_path in jpg_paths {
+        let arg_input = arg_input.clone();
+        let bar_clone = bar.clone();
+        let handle = thread::spawn(move || {
+            let jpg = jpg_path.to_str().expect("couldn't unwrap jpg_path");
+            let exif_args = [
+                "-quiet",
+                "-overwrite_original",
+                "-ee",
+                "-TagsFromFile",
+                arg_input.as_str(),
+                "-all:all",
+                "-unsafe",
+                "-icc_profile",
+                jpg,
+            ];
+            let output = Command::new("exiftool")
+                .args(exif_args)
+                .output()
+                .expect("failed to run exiftool");
+            if !output.status.success() {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                eprintln!("exiftool error on {}: {}", jpg, stderr);
+            }
+            bar_clone.inc(1);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().expect("an exiftool thread panicked!")
+    }
+
+    bar.finish_with_message("metadata extraction complete!");
+    println!("\n\nframes written to {}.\n\ndone!\n", output_dir.display());
 
     Ok(())
 }
